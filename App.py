44e8db612c394914e065da7ae1e5524952bb2de8
Anirban Tudu <129039794+anirbantudu2003@users.noreply.github.com>
2025-10-06 20:19:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import pickle as pkl 
 import streamlit as st
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
@@ -47,6 +46,7 @@ if st.button('Predict'):
     display_string = 'Insurance Premium will be '+ str(round(predicted_prem[0],2)) + ' Indian Rupees'
 
     st.markdown(display_string)
+
 
 
 
